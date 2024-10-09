0a2a546afeccc36ae71a3a5f18194375e6ea07a8
darvin-francis <darvinfrancis30@gmail.com>
2024-10-09 14:30:58 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,18 @@
-
+StringCalculator.h 
+ 
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+ 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <regex>
+ 
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+};
+ 
+#endif
